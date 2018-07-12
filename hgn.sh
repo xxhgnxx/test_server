0fb760ac0e85f3c20f9da2b1ff8f089344f59f70
胡功宁 <hugongning@dongqiudi.com>
2018-07-12 15:30:06 +0800

    save

--- hgn.sh
+++ hgn.sh
@@ -10,13 +10,16 @@ err(){
 
 current_branch=`git symbolic-ref --short -q HEAD`
 target=develop
-if   [ $current_branch = 'develop' || $current_branch = 'beta'] 
+if   [ $current_branch = "develop" ]  || [ $current_branch = 'beta' ] 
 then
-  err '当前分支为'$target
+  err '当前分支为'$current_branch
   err '请检查当前分支后再做合并'
   exit
 fi
 
+add_results = `git add .`
+add_results = `git commit -m save`
+# echo add_results
 echo -e "当前分支 \033[33m ${current_branch} \033[0m --> \033[33m ${target} \033[0m"
 echo -e "add"
 
