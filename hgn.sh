e7b81e1fa6542457db269ae032a1fdd5cd145e5c
胡功宁 <hugongning@dongqiudi.com>
2018-07-12 14:49:05 +0800

    save

---
+++ hgn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+current_branch=`git symbolic-ref --short -q HEAD`
+current_branch=`git symbolic-ref --short -q HEAD`
+current_branch=`git symbolic-ref --short -q HEAD`
+current_branch=`git symbolic-ref --short -q HEAD`
+
+
+echo "$current_branch"
+echo "$current_branch"
+echo "$current_branch"
+echo "$current_branch"
+
